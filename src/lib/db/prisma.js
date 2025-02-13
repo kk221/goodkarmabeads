1c3d3ca6634adba9415fd4e74e4532e8e5fb046a
Kexin <liukexin1990@gmail.com>
2025-02-13 17:35:36 +0100

    Add Prisma configuration and API routes

--- src/lib/db/prisma.js
+++ src/lib/db/prisma.js
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-// Remove TypeScript syntax and use regular JavaScript
-const globalForPrisma = global
+let prisma
 
-// Check if prisma exists in the global object
-if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = new PrismaClient()
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
 }
-
-export const prisma = globalForPrisma.prisma
 
 export default prisma
